7bcb03459f96f02e1e1450405fcd161354b8cf65
Eumericano <51465561+EumYoungHyun@users.noreply.github.com>
2022-01-15 16:20:58 +0900

    feat: 메뉴 레이아웃 설정

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { COLOR, FONT_WEIGHT } from "./constants";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <StatusBar style="light" />
+            <View style={styles.header}>
+                <Text style={styles.btnText}>Work</Text>
+                <Text style={styles.btnText}>Learn</Text>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: COLOR.BLACK,
+        paddingHorizontal: 20,
+    },
+    header: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 100,
+    },
+    btnText: {
+        fontSize: 38,
+        fontWeight: FONT_WEIGHT.BOLD,
+        color: COLOR.WHITE,
+    },
 });
