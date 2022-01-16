0e6c63ff759ee50a0e40b3717acc33dc3315401a
Eum Young Hyun <opwer032@naver.com>
2022-01-16 21:59:22 +0900

    feat: Touchable 추가, 클릭시 문자열 색 변경

--- App.tsx
+++ App.tsx
@@ -1,33 +1,56 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { COLOR, FONT_WEIGHT } from "./constants";
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <StatusBar style="light" />
-            <View style={styles.header}>
-                <Text style={styles.btnText}>Work</Text>
-                <Text style={styles.btnText}>Learn</Text>
-            </View>
-        </View>
-    );
+  const [working, setWorking] = useState<boolean>(true);
+
+  const setToLearn = () => setWorking(false);
+  const setToWork = () => setWorking(true);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={setToWork}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: working ? COLOR.WHITE : COLOR.GREY,
+            }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={setToLearn}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: working ? COLOR.GREY : COLOR.WHITE,
+            }}
+          >
+            Learn
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: COLOR.BLACK,
-        paddingHorizontal: 20,
-    },
-    header: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        marginTop: 100,
-    },
-    btnText: {
-        fontSize: 38,
-        fontWeight: FONT_WEIGHT.BOLD,
-        color: COLOR.WHITE,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: COLOR.BLACK,
+    paddingHorizontal: 20,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: FONT_WEIGHT.BOLD,
+  },
 });
