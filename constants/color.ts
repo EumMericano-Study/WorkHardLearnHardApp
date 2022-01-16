8572e6a2439ff302e3c7f4f10354035976d299d8
Eum Young Hyun <opwer032@naver.com>
2022-01-16 23:45:43 +0900

    feat: 일반 회색 색상 추가

--- constants/color.ts
+++ constants/color.ts
@@ -1,7 +1,8 @@
 const COLOR = {
-    WHITE: "#f2f2f2",
-    BLACK: "#070707",
-    GREY: "#3A3D40",
+  WHITE: "#f2f2f2",
+  BLACK: "#070707",
+  DARK_GREY: "#3A3D40",
+  GREY: "#5C5C60",
 };
 
 export default COLOR;
